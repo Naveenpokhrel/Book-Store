53b6b90cc8b7b89d952c64daa742d806980fee15
Naveenpokhrel <naveenpokhrel99@gmail.com>
2025-10-15 15:19:04 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Toaster} from "react-hot-toast"
+import { Routes, Route } from 'react-router-dom'
+const App = () => {
+  return (
+    <main>
+      <Toaster position='bottom-right' />
+      <Routes>
+        
+      </Routes>
+    </main>
+  )
+}
+
+export default App
---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductDetails = () => {
+  return (
+    <div>ProductDetails pages</div>
+  )
+}
+
+export default ProductDetails
